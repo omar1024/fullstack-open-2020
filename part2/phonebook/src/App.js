3f47da6c79fb852c463ee91f4675ebac1fdbff4e
omar <umarfarooqa16@gmail.com>
2020-11-17 21:24:26 +0530

    2.5

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const personsList = persons.map(person => {
+    return <Person key={person.name} name={person.name} />;
+  });
+
+  const handleChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const person = {
+      name: newName
+    };
+
+    setPersons([...persons].concat(person));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={event => handleSubmit(event)}>
+        <div>
+          name:{" "}
+          <input onChange={event => handleChange(event)} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsList}
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Person = props => {
+    return (
+      <>
+        {props.name}
+        <br />
+      </>
+    );
+  };
+
+export default Person;
