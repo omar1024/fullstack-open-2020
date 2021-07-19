412d6c3190abfda9f5ec8d6233a2570bd971de16
omar1024 <omar1024@protonmail.com>
2021-07-19 12:49:04 +0530

    3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -37,6 +37,18 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${length} people</p> <p>${new Date}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  console.log(person)
+  if(person){
+    response.json(person)
+  }else{
+    response.statusCode(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
