0b0a1268ffda857c18d8b142e266607aa6b80537
omar1024 <omar1024@protonmail.com>
2021-07-20 04:35:24 +0530

    3.6 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { request, response } from 'express'
 const app = express()
+app.use(express.json())
 
 let persons = [
   { 
@@ -25,7 +26,7 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hell rld!</h1>')
+  response.send('<h1>dummy checks</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
@@ -49,6 +50,37 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id',(request,response)=>{
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  console.log("sending req")
+  const body = request.body
+
+  if (!body.name || !body.number) {
+      return response.status(400).json({
+          error: 'name or number is missing'
+      })
+  } else if (persons.find(person => person.name === body.name)) {
+      response.status(400).json({
+          error: 'name must be unique'
+      })
+  }
+
+  const person = {
+      id: Math.floor(Math.random() * 9999),
+      name: body.name,
+      number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
